627c9acb74d060670464f65a3de5ef03444ce6a6
gvincenti <vgonzalo2014@gmail.com>
2024-07-15 11:50:39 -0300

    aver

--- public/js/product.js
+++ public/js/product.js
@@ -21,7 +21,7 @@ const swiper = new Swiper('.mySwiper', {
 
 const getProducts = async () => {
   try {
-    const results = await fetch('./data/products.json');
+    const results = await fetch('./data/products');
     const data = await results.json();
     const products = data.products;
     return products;
